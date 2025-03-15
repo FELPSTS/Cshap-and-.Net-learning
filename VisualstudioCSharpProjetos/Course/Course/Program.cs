63b294bd7a3998fe1d2014a28862040ec285c52b
FELPSTS <macleimotog0@gmail.com>
2025-03-14 23:38:58 -0300

    second class

---
+++ VisualstudioCSharpProjetos/Course/Course/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            byte n1 = 255;
+
+            n1++;
+
+            Console.WriteLine(n1);
+
+            if (n1 == 0) 
+            {
+                Console.WriteLine("passou do limite de byts");
+                    } ;
+        }
+    }
+}

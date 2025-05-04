4f3771a0eac077dc0ffe34a8de90ac06763a95c1
FELPSTS <macleimotog0@gmail.com>
2025-05-03 21:10:53 -0300

    exemple of triangle

---
+++ triangle/triangle/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double xA, xB, xC, yA, yB, yC;
+            Console.WriteLine("Digite os valores do triangulo A");
+            xA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            xB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            xC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Digite os valores do triangulo B");
+            yA = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            yB = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            yC = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double p = (xA + xB + xC) / 2.0;
+            double areaA = Math.Sqrt(p * (p - xA) * (p - xB) * (p - xC));
+
+            p = (yA + yB + yC) / 2.0;
+            double areaB = Math.Sqrt(p * (p - yA) * (p - yB) * (p - yC));
+
+            Console.WriteLine("Area do triangulo A: " + areaA.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Area do triangulo B: " + areaB.ToString("F4", CultureInfo.InvariantCulture));
+
+            if (areaA > areaB)
+            {
+                Console.WriteLine("O triangulo A tem a maior area");
+            }
+            else
+            {
+                Console.WriteLine("O triangulo B tem a maior area");
+            }
+        }
+    }
+}

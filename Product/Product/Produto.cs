b318443c3f1ff6efaee5bfaea752b6944f78785f
FELPSTS <macleimotog0@gmail.com>
2025-05-09 12:58:40 -0300

    Product class

---
+++ Product/Product/Produto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Product
+{
+    class Produto
+    {
+       public String nome;
+       public String Preco;
+       public String Quatindade;
+    
+        public Double valorTotalEmEstoque()
+        {
+            return Double.Parse(Preco) * Double.Parse(Quatindade);
+        }
+    }
+    
+}
---
+++ Product/Product/Program.cs
@@ -0,0 +1,27 @@
+﻿using Product;
+using System;
+using System.Globalization;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Produto p = new Produto();
+
+            Console.WriteLine("Digite os dados do produto");
+            Console.WriteLine("Digite o nome do produto");
+            p.nome = Console.ReadLine();
+            Console.WriteLine("Digite o preço do produto");
+            p.Preco = Console.ReadLine();
+
+            Console.WriteLine("Digite a quantidade do produto no estoque");
+            p.Quatindade = Console.ReadLine();
+
+            Console.WriteLine("Dados do produto: " + p.nome + ", " + p.Preco + ", " + p.Quatindade);
+
+        }
+    } 
+}

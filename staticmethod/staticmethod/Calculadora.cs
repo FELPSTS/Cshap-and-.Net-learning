8831de48dff430604a18852d1141eff9b512cf42
FELPSTS <macleimotog0@gmail.com>
2025-05-10 12:51:24 -0300

    sophisticated instance

--- staticmethod/staticmethod/Calculadora.cs
+++ staticmethod/staticmethod/Calculadora.cs
@@ -2,13 +2,13 @@
 {
     class Calculadora
     {    
-        public double Pi = 3.14;
+        public static double Pi = 3.14;
 
-        public double Circunferencia(double r)
+        public static double Circunferencia(double r)
         {
             return 2.0 * Math.PI * r;
         }
-        public double Volume(double r)
+        public static double Volume(double r)
         {
             return 4.0 / 3.0 * Math.PI * Math.Pow(r, 3);
         }
--- staticmethod/staticmethod/Program.cs
+++ staticmethod/staticmethod/Program.cs
@@ -8,17 +8,16 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            Calculadora calc = new Calculadora();
-
+    
             Console.WriteLine("Entre o valor do raio: ");
             double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
-            double circ = calc.Circunferencia(raio);
-            double volume = calc.Volume(raio);
+            double circ = Calculadora.Circunferencia(raio);
+            double volume = Calculadora.Volume(raio);
 
             Console.WriteLine("Circunferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
             Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
-            Console.WriteLine("Valor de Pi: " + calc.Pi.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor de Pi: " + Calculadora.Pi.ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }
